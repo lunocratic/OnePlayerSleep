8c108b3f4f5ae08e5d3a997e1a9e95fea4db703e
Ashok Manikanta <129861436+ashok0076@users.noreply.github.com>
2025-05-06 18:59:26 +0530

    Done

---
+++ MCPlugin/OnePlayerSleep/src/main/java/me/yourname/oneplayersleep/App.java
@@ -0,0 +1,13 @@
+package me.yourname.oneplayersleep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
